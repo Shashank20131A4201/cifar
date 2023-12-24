d93d6ccffe91535c08c5c87ee308cb715ac33138
Shashank 20131A4201 <20131a4201@gvpce.ac.in>
2023-12-24 07:12:44 +0000

    24-12-23 12:42

---
+++ fast.py
@@ -0,0 +1,49 @@
+import uvicorn
+from fastapi import FastAPI, UploadFile, File, Request,Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+import io
+from PIL import Image
+import os
+import cv2
+import shutil
+from process import process_and_return
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory = "static"), name = "static")
+
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def read_root():
+    with open("templates/index.html", 'r') as file:
+        content = file.read()
+    return HTMLResponse(content=content)
+
+@app.post("/upload")
+async def upload_image(image: UploadFile):
+    if image.filename != '':
+        # Save the uploaded image to the specified directory
+        image_path = os.path.join(UPLOAD_FOLDER, image.filename)
+        with open(image_path, "wb") as f:
+            shutil.copyfileobj(image.file, f)
+
+        # Process the uploaded image using Pillow (PIL)
+        img = Image.open(image_path)
+        predictions = process_and_return(image_path)
+
+        return {"message": "Image uploaded and processed successfully.","Pred":predictions}
+    return {"error": "No image selected."}
+
+if __name__ == '__main__':
+   uvicorn.run(app, host='0.0.0.0', port=8000)
+
+
+   #apt-get install -y libgl1-mesa-dev
+   #python3 -m pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.12.0-py3-none-any.whl
---
+++ process.py
@@ -0,0 +1,26 @@
+import uvicorn
+from fastapi import FastAPI, UploadFile, File
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+import io
+from PIL import Image
+import cv2
+import numpy as np
+from keras.models import load_model
+
+#file_path="static/download.png"
+def process_and_return(file_path):
+  image = cv2.imread(file_path)
+  print("Image shape:", image.shape)
+  resized_image = cv2.resize(image, (32, 32))
+  input_data = np.expand_dims(resized_image, axis=0)
+  input_data=input_data/255.0
+  model = load_model('modell.h5')
+  classes = ["airplane", "automobile" , "bird", "cat", "deer", "dog", "frog", "horse", "ship",  "truck"]
+  predictions=model.predict(input_data)
+  return classes[predictions.argmax()]
+  
+
+# classes = ["airplane", "automobile" , "bird", "cat", "deer", "dog", "frog", "horse", "ship",  "truck"]
+# print("Class of the Image : ", classes[process_and_return(file_path)])
