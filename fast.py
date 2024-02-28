b7bf771d79b0007c99307c87f293ff5ae44147ad
Shashank 20131A4201 <20131a4201@gvpce.ac.in>
2024-02-28 10:04:46 +0000

    First Version Deployed

--- fast.py
+++ fast.py
@@ -21,25 +21,33 @@ if not os.path.exists(UPLOAD_FOLDER):
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def read_root():
-    with open("templates/index.html", 'r') as file:
-        content = file.read()
-    return HTMLResponse(content=content)
+def read_root(request: Request):
+    # with open("templates/index.html", 'r') as file:
+    #     content = file.read()
+    # return HTMLResponse(content=content)
+
+    return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post("/upload")
-async def upload_image(image: UploadFile):
+async def upload_image(request: Request,image: UploadFile):
     if image.filename != '':
-        # Save the uploaded image to the specified directory
+        ## Saving the uploaded image to the specified directory
         image_path = os.path.join(UPLOAD_FOLDER, image.filename)
         with open(image_path, "wb") as f:
             shutil.copyfileobj(image.file, f)
 
-        # Process the uploaded image using Pillow (PIL)
+        ## Processing the uploaded image 
         img = Image.open(image_path)
         predictions = process_and_return(image_path)
 
-        return {"message": "Image uploaded and processed successfully.","Pred":predictions}
-    return {"error": "No image selected."}
+    #     return {"message": "Image uploaded and processed successfully.","Pred":predictions}
+    # return {"error": "No image selected."}
+
+        return templates.TemplateResponse("index.html", {"request": request, "message": "Image uploaded and processed successfully.", "Pred": predictions})
+    return templates.TemplateResponse("index.html", {"request": request, "error": "No image selected."})
+
+
 
 if __name__ == '__main__':
    uvicorn.run(app, host='0.0.0.0', port=8000)
